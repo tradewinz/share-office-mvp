36fb07b2661ac3e6a8064e9bfe9a53ce4a6bd881
kghosh <kaushik_ghosh@intuit.com>
2013-05-01 16:34:22 -0700

    drop listings table

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130430215906) do
-
-  create_table "listings", :force => true do |t|
-    t.string   "description"
-    t.string   "location"
-    t.string   "headline"
-    t.integer  "user_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.string   "image"
-  end
+ActiveRecord::Schema.define(:version => 20130501225237) do
 
   create_table "offices", :force => true do |t|
     t.decimal  "rent"
