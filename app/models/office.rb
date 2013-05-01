c0d71bce056eb4e306606cfa79433f2b93621bde
kghosh <kaushik_ghosh@intuit.com>
2013-05-01 15:20:10 -0700

    Fix auth check in UI

--- app/models/office.rb
+++ app/models/office.rb
@@ -2,7 +2,7 @@ class Office < ActiveRecord::Base
   attr_accessible :availability, :company_name, :company_desc, :company_url, :contact_email, :contact_name, :contact_phone,
                   :contact_picture, :description, :image1, :image2, :image3, :image4, :insurance, :rent, :size, :swap,
                   :title, :years, :accomodate, :available, :loc_zip, :loc_addr1, :loc_addr2, :loc_city, :loc_state,
-                  :features, :on_site
+                  :features, :on_site, :user_id
 
   belongs_to :user                  
   validates(:title, presence: true)
