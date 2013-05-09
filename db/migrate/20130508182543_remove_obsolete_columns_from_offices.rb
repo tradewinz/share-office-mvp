8c590530cca087cda63a148fa347e9f02f4e25ca
kghosh <kaushik_ghosh@intuit.com>
2013-05-08 17:12:02 -0700

    fix migration script

--- db/migrate/20130508182543_remove_obsolete_columns_from_offices.rb
+++ db/migrate/20130508182543_remove_obsolete_columns_from_offices.rb
@@ -11,7 +11,6 @@ class RemoveObsoleteColumnsFromOffices < ActiveRecord::Migration
     remove_column :offices, :years
     remove_column :offices, :contact_email
     remove_column :offices, :contact_phone
-    remove_column :offices, :company_name
     remove_column :offices, :features
     remove_column :offices, :on_site
   end
@@ -19,7 +18,6 @@ class RemoveObsoleteColumnsFromOffices < ActiveRecord::Migration
   def down
     add_column :offices, :on_site, :boolean
     add_column :offices, :features, :text
-    add_column :offices, :company_name, :string
     add_column :offices, :contact_phone, :string
     add_column :offices, :contact_email, :string
     add_column :offices, :years, :integer
