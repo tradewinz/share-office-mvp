706a2527020fe6173e38bd41cabc0175dd822d10
kghosh <kaushik_ghosh@intuit.com>
2013-05-24 17:45:45 -0700

    final touches

--- app/mailers/office_mailer.rb
+++ app/mailers/office_mailer.rb
@@ -3,13 +3,13 @@ class OfficeMailer < ActionMailer::Base
   
   def add_listing_confirm(office)
   	@office = office
-  	mail(:to => office.user.email, :subject => "Your listing was posted with tradewinz")
-    mail(:to => "tradewinz@intuit.com", :subject => "High-Five! Someone just added a listing at tradewinz")
+  	mail(:to => office.user.email, :subject => "Your listing was posted with Intuit Spaces")
+    mail(:to => "spaces@intuit.com", :subject => "High-Five! Someone just added a listing @ Spaces")
   end
 
   def edit_listing_confirm(office)
     @office = office
-    mail(:to => "tradewinz@intuit.com", :subject => "Someone just edited their listing at tradewinz")
+    mail(:to => "spaces@intuit.com", :subject => "Someone just edited their listing")
   end
 
 end
