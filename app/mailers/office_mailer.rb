5567ca38b3d9134be9aadcd74b6dd9f6db70ecb8
kghosh <kaushik_ghosh@intuit.com>
2013-05-31 09:58:09 -0700

    remove migration and stop emailing

--- app/mailers/office_mailer.rb
+++ app/mailers/office_mailer.rb
@@ -4,18 +4,18 @@ class OfficeMailer < ActionMailer::Base
   def add_listing_confirm(office)
   	@office = office
   	mail(:to => office.user.email, :subject => "Your listing was posted with Intuit Spaces")
-    mail(:to => "kaushik_ghosh@intuit.com", :subject => "High-Five! Someone just added a listing @ Spaces")
+    mail(:to => "spaces@intuit.com", :subject => "High-Five! Someone just added a listing @ Spaces")
   end
 
   def edit_listing_confirm(office)
     @office = office
-    mail(:to => "kaushik_ghosh@intuit.com", :subject => "Someone just edited their listing")
+    mail(:to => "spaces@intuit.com", :subject => "Someone just edited their listing")
   end
 
   def reserve_listing_confirm(office, current_user)
     @office = office
     @current_user = current_user
     mail(:to => office.user.email, :subject => "Someone just reserved your listing! ")
-    mail(:to => "kaushik_ghosh@intuit.com", :subject => "High-Five! Someone just added reserved a listing")
+    mail(:to => "spaces@intuit.com", :subject => "High-Five! Someone just added reserved a listing")
   end
 end
