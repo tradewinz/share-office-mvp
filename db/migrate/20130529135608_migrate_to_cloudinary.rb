294d4006ad0e28da680841ddd0545e89410ea87a
Siddharth Ram <siddharth_ram@intuit.com>
2013-05-30 18:23:58 -0700

    fixed attachinery, added preview images

--- db/migrate/20130529135608_migrate_to_cloudinary.rb
+++ db/migrate/20130529135608_migrate_to_cloudinary.rb
@@ -1,16 +1,16 @@
 class MigrateToCloudinary < ActiveRecord::Migration
   def up
- 	s3 = AWS::S3.new(:access_key_id => 'AKIAJWKNWRO6ZAWT74HA', 
+# 	s3 = AWS::S3.new(:access_key_id => 'AKIAJWKNWRO6ZAWT74HA', 
  					 :secret_access_key => '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')
 
- 	s3.buckets.each do |bucket| 
- 		puts "bucket = " + bucket.name
- 		bucket.objects.each(:limit => 20) do |image|
- 			url = "https://" + bucket.name + ".s3.amazonaws.com/" + image.key 
- 			puts image.key + " path = " + url
- 			Cloudinary::Uploader.upload(url)
- 		end
- 	end
+# 	s3.buckets.each do |bucket| 
+# 		puts "bucket = " + bucket.name
+# 		bucket.objects.each(:limit => 20) do |image|
+# 			url = "https://" + bucket.name + ".s3.amazonaws.com/" + image.key 
+# 			puts image.key + " path = " + url
+# 			Cloudinary::Uploader.upload(url)
+ #		end
+# 	end
 end
 
 
