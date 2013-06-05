9433f5c2d37d225d84ba03ab82ade7c457677dc8
kghosh <kaushik_ghosh@intuit.com>
2013-06-05 12:39:52 -0700

    final final checkin

--- app/mailers/office_mailer.rb
+++ app/mailers/office_mailer.rb
@@ -3,23 +3,23 @@ class OfficeMailer < ActionMailer::Base
 
   def add_listing_confirm(office)
   	@office = office
-  	mail(:to => office.user.email, :subject => "Your listing was successfully posted at Intuit Spaces")
+  	mail(:to => office.user.email, :subject => "Your listing was successfully posted")
   end
 
   def edit_listing_confirm(office)
     @office = office
-    mail(:to => office.user.email, :subject => "Your listing at Intuit Spaces was updated successfully")
+    mail(:to => office.user.email, :subject => "Your listing was updated successfully")
   end
 
   def reserve_listing_confirm(office, current_user)
     @office = office
     @current_user = current_user
-    mail(:to => office.user.email, :subject => "Someone just reserved your listing at Intuit Spaces!")
+    mail(:to => office.user.email, :subject => "Someone wants to rent your space!")
   end
 
   def request_reserve_confirm(office, current_user)
     @office = office
     @current_user = current_user
-    mail(:to => current_user.email, :subject => "Thank you for booking a listing at Intuit Spaces!")
+    mail(:to => current_user.email, :subject => "Your request to contact the host has been sent")
   end
 end
