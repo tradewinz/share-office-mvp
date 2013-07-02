a90ed74af3795ccfe2ab3c32cc28f9f8c6867b2b
kghosh <kaushik_ghosh@intuit.com>
2013-07-02 14:56:27 -0700

    moving to nominatim from google

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,11 +1,12 @@
 Geocoder.configure(
-    # google free service
+    # open source free geocode service
     :lookup => :nominatim
-    # :lookup => :google
+    # DO NOT USE THE FREE GEOCODE SERVICE< YOU WILL GET OVER QUOTA SOON AT AMWS
+    #:lookup => :google
 
     # Google premier geocoding service
-    :lookup => :google_premier,
+    #:lookup => :google_premier,
 
     # Intuit enterprise license, must be on intuit.com domain [key, client, channel]
-    :api_key => ["???", "gme-intuitinc", "fms.spaces"]
+    #:api_key => ["???", "gme-intuitinc", "fms.spaces"]
 )
