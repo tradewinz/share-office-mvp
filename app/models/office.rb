aff8b0abccf64028fdfafc4ebd57a306a238f663
Alice Fang <fangx@stanford.edu>
2013-07-09 15:21:45 -0700

    added navigation

--- app/models/office.rb
+++ app/models/office.rb
@@ -33,20 +33,20 @@ class Office < ActiveRecord::Base
     [loc_addr1, loc_city, loc_state, loc_zip].compact.join(', ')
   end
 
-  # def next(query)
-  #   unless query.nil?
-  #     index = query.find_index(self.id)
-  #     next_id = query[index+1] unless index == query.size
-  #     self.class.find_by_id(next_id)
-  #   end
-  # end
+  def next(query)
+    unless query.nil?
+      index = query.find_index(self.id)
+      next_id = query[index+1] unless index == query.size
+      self.class.find_by_id(next_id)
+    end
+  end
 
-  # def previous(query)
-  #   unless query.nil?
-  #     index = query.find_index(self.id)
-  #     prev_id = query[index-1] unless index.zero?
-  #     self.class.find_by_id(prev_id)
-  #   end
-  # end
+  def previous(query)
+    unless query.nil?
+      index = query.find_index(self.id)
+      prev_id = query[index-1] unless index.zero?
+      self.class.find_by_id(prev_id)
+    end
+  end
 
 end
