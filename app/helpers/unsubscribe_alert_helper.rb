829256d4dc898213db5fdc1cb24d92692ed6f860
Alice Fang <xalicefang@gmail.com>
2013-08-06 13:27:40 -0700

    bug fix

--- app/helpers/unsubscribe_alert_helper.rb
+++
@@ -1,2 +0,0 @@
-module UnsubscribeAlertHelper
-end
--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,6 +1,5 @@
 class Alert < ActiveRecord::Base
   attr_accessible :city, :email, :misc
-  has_many :unsubscribe_alert
 
   validates :city, :presence => true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
