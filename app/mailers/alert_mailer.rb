5747eb5dd01ac22ab886ca14086b3499899e6576
Alice Fang <xalicefang@gmail.com>
2013-08-06 09:43:44 -0700

    alerts mailer but now alerts module doesn't open

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -1,6 +1,12 @@
 class AlertMailer < ActionMailer::Base
   self.default :from => 'Intuit Spaces <spaces@intuit.com>'
 
+  def confirm(alert)
+  	@alert = alert
+  	mail(:to => alert.email, :subject => "Thank you for signing up for #{alert.city} alerts!")
+  end
+
+
   def match_found(office,alert)
   	@office = office
   	@alert = alert
