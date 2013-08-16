211fa18d0ef2ac3abfde0e5b3fcc13114d4d2905
Alice Fang <xalicefang@gmail.com>
2013-08-16 15:20:58 -0700

    featured navigation

--- app/controllers/offices_controller.rb
+++ app/controllers/offices_controller.rb
@@ -18,7 +18,7 @@ class OfficesController < ApplicationController
     #@reserf = Reserve.new
 
     # make sure next and prev are only found when searching; if not search, clear session
-    if (session[:query] and session[:search_results] and session[:query].include? @office.id and (request.referer.present? and (request.referer.include? "offices/" or request.referer.include? "listings"))) or Rails.application.routes.recognize_path(request.referrer)==Rails.application.routes.recognize_path(home_path)
+    if (session[:query] and session[:search_results] and session[:query].include? @office.id and (request.referer.present? and (request.referer.include? "offices/" or request.referer.include? "listings" or Rails.application.routes.recognize_path(request.referrer)==Rails.application.routes.recognize_path(home_path))))
       @next_office = @office.next(session[:query])
       @prev_office = @office.previous(session[:query])
     end 
