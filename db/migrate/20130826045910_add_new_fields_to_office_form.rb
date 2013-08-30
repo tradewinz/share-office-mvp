dfa2c8f775f700ac0ebee27bd015886d2e1a0601
Kaushik Ghosh <ghosh99@yahoo.com>
2013-08-29 17:17:20 -0700

    stage rollback fix

--- db/migrate/20130826045910_add_new_fields_to_office_form.rb
+++ db/migrate/20130826045910_add_new_fields_to_office_form.rb
@@ -1,6 +1,5 @@
 class AddNewFieldsToOfficeForm < ActiveRecord::Migration
    def change
-    add_column :offices, :type_office, :integer, :default => 1
     add_column :offices, :min_lease, :integer, :default => 1
     add_column :offices, :security, :boolean
     add_column :offices, :cleaning, :boolean
